f81abd5617135a059d04bd34e4a5a5527b50f840
gogoat35 <127574271+gogoat35@users.noreply.github.com>
2023-12-04 17:11:32 -0600

    Update title.js

--- title.js
+++ title.js
@@ -16,7 +16,7 @@ function loaded() {
 
 var x = 0;
 
-var titleText = ["DOWNLOAD", "TAMPER" , ];
+var titleText = ["DOWNLOAD", "TAMPER" , "THEN", "JOIN", "THE", "DISCORD", "SERVER",];
 
 function loop() {
 
